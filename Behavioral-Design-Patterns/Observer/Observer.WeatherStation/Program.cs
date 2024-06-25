9411812be3a5595194b96bf7674b49702462e037
samianzahra <samianzahra@gmail.com>
2024-06-26 00:34:08 +0330

    README

--- Behavioral-Design-Patterns/Observer/Observer.WeatherStation/Program.cs
+++ Behavioral-Design-Patterns/Observer/Observer.WeatherStation/Program.cs
@@ -6,6 +6,8 @@ weatherStation.Add(new Kiosk("8.7.5.3"));
 weatherStation.Add(new Kiosk("22.11.22.33"));
 weatherStation.Add(new TVRoom());
 
+
+
 weatherStation.SetTemprature(10);
 
 weatherStation.Add(new WallTV());
